784ef6893c1553c012d2f2f9c73cac5c67dfebfd
jaisheel <jaisheel021@gmail.com>
2026-01-01 20:33:49 +0530

    setup up tailwindcss , daisy ui, iconify

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-amber-600">Welcome to Travel Logger</h1>
+  <button class="btn btn-active btn-warning">
+    View Map <Icon name="tabler:map" />
+
+  </button>
   </div>
 </template>
